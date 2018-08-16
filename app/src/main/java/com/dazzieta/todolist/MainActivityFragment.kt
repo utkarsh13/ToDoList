79989e39fee7b1384ca7265558a34338907b5715
utkarshr <utkarsh.rastogi@citrix.com>
2018-08-16 20:52:21 +0530

    Added basic recycler view to fragment

--- app/src/main/java/com/dazzieta/todolist/MainActivityFragment.kt
+++ app/src/main/java/com/dazzieta/todolist/MainActivityFragment.kt
@@ -2,9 +2,15 @@ package com.dazzieta.todolist
 
 import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dazzieta.todolist.model.TodoItem
+import kotlinx.android.synthetic.main.todo_list_item.view.*
+import kotlinx.android.synthetic.main.todo_recycler_view.*
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,9 +20,39 @@ class MainActivityFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
+        val view = inflater.inflate(R.layout.todo_recycler_view, container, false)
 
+        val todoRecyclerView = view.findViewById(R.id.todoRecyclerView) as RecyclerView
+        todoRecyclerView.layoutManager = LinearLayoutManager(activity)
+        val adapter = TodoAdapter(todoList = listOf(TodoItem("aaa", 123), TodoItem("bbb", 234)), context = activity)
+        todoRecyclerView.adapter = adapter
 
-
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return view
     }
+
+    class TodoViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(todoItem: TodoItem){
+            itemView.todoData.text = todoItem.todo
+            itemView.todoTime.text = todoItem.time.toString()
+        }
+    }
+
+    class TodoAdapter(val todoList: List<TodoItem>, val context: FragmentActivity?) : RecyclerView.Adapter<TodoViewHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+            return TodoViewHolder(LayoutInflater.from(context).inflate(R.layout.todo_list_item, parent, false))
+        }
+
+        override fun getItemCount(): Int {
+            return todoList.size
+        }
+
+        override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+            holder.bind(todoList.get(position))
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/dazzieta/todolist/model/TodoItem.kt
@@ -0,0 +1,3 @@
+package com.dazzieta.todolist.model
+
+data class TodoItem(var todo : String, var time : Long)
